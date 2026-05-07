1e99e4001dfa5ac364e8d33d883df801d3f39ed5
shahdalhosani <salhosanii227@gmail.com>
2026-05-07 23:45:48 +0400

    Create and Run Main Window

---
+++ code/24_main_program.py
@@ -0,0 +1,14 @@
+#Create the main window
+window = tk.Tk()
+
+#Set title
+window.title("Smart Campus Facility Booking System - Zayed University")
+
+#Set size
+window.geometry("750x650")
+
+#Start with main menu
+main_menu()
+
+#Keep window open
+window.mainloop()
